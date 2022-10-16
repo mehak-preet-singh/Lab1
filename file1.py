d87fa1c91b657e6825953b2a90b15bc6540257e7
mehak-preet-singh <115911437+mehak-preet-singh@users.noreply.github.com>
2022-10-16 19:43:57 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,5 @@
 "A function that takes an input from the user and calculate someone’s age":
+#!/usr/bin/env/python3  
 
 Year= int(input("Enter the year in you born"))
 Month= int(input("Enter your number of birthday month"))
